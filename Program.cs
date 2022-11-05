80054a49ddb38f38cc872562069867cfcc3befa1
Pedro Henrique <PedroHenrique053010@gmail.com>
2022-11-04 21:26:58 -0300

    adiciona a resolução ao exercicio

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿int numeroInicial = 0;
+System.Console.Write(" Numeros pares entre 0 - ");
+int numeroLimite = Convert.ToInt32(Console.ReadLine());
+
+Console.ForegroundColor = ConsoleColor.Magenta;
+while (numeroInicial <= numeroLimite)
+
+{
+  System.Console.Write($" {numeroInicial} " );
+  numeroInicial += 2;
+
+}
+
+Console.ResetColor();
+
+System.Console.WriteLine();
+
